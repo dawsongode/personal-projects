f27e799780d77fc44ca4a7a8825310000115cd72
Dawson Gode <dbgode@iastate.edu>
2022-09-21 18:16:34 -0500

    pluralsight

---
+++ personal-projects/pluralsight/addition.py
@@ -0,0 +1,11 @@
+# Creating the addition function
+def addition(a,b):
+    return a + b
+
+# main program
+num1 = float(input("Enter your 1st number:\n"))
+num2 = float(input("Enter your 2nd number:\n"))
+
+# Calling our function
+result = addition(num1, num2)
+print("the result is", result)
